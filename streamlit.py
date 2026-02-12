af430ffde9e4b4c5df6bfec096dcee4f012b3a6c
mckoh <michael.kohlegger@gmail.com>
2026-02-12 14:50:20 +0100

    changed text spacing in embedding plot

--- streamlit.py
+++ streamlit.py
@@ -191,7 +191,7 @@ with tab3:
     ax.legend(loc=0)
 
     for i, word in enumerate(pp.encoder.categories_[0]):
-        plt.text(x=w1[0,i]+0.01, y=w1[1,i]+0.01, s=word)
+        plt.text(x=w1[0,i]+0.05, y=w1[1,i]+0.05, s=word)
 
     ax.set_ylabel("weights to hidden 2")
     ax.set_xlabel("weights to hidden 1")
